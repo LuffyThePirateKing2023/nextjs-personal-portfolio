45141e5df9e9ef62a9ed7b92b07685f268569cb7
JKOfficialrepository <150002044+JKOfficialrepository@users.noreply.github.com>
2024-06-25 17:36:46 +0800

    add logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body
         className={clsx(
           inter.variable,
