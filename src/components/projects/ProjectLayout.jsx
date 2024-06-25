5f66bf6bbe39621ea710126e0fe8bc22f4fb6176
JKOfficialrepository <150002044+JKOfficialrepository@users.noreply.github.com>
2024-06-25 16:45:54 +0800

    udpate: project layout

--- src/components/projects/ProjectLayout.jsx
+++ src/components/projects/ProjectLayout.jsx
@@ -7,17 +7,24 @@ const item = {
 };
 
 const ProjectLink = motion(Link);
-const ProjectLayout = ({ name, description,demoLink, sourceCode }) => {
+const ProjectLayout = ({ name, description, demoLink, sourceCode }) => {
   return (
     <ProjectLink
       variants={item}
       href={demoLink}
-      target={"_blank"}
-      className=" text-sm md:text-base flex  items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg"
+      target="_blank"
+      className="text-sm md:text-base flex items-center justify-center w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg"
     >
-      <div className="flex items-center justify-evenly space-x-2">
-        <h2 className="text-foreground">{name}</h2>
-        <p className="text-muted hidden sm:inline-block">{description}</p> 
+      <div className="flex items-center justify-between space-x-2 w-full ">
+        <div className="flex items-center justify-center w-4/5">
+          <h2 className="text-foreground w-2/5">{name}</h2>
+          <p className="w-3/5 text-muted hidden sm:inline-block">{description}</p>
+        </div>
+        <Link href={sourceCode} target="_blank" passHref>
+          <p className="text-foreground">
+            Source Code
+          </p>
+        </Link>
       </div>
     </ProjectLink>
   );
